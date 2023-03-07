7f3ff0ecfe3bb9581db99b25f3ed21da783b15b3
Prepod <vlad.it.instructor@gmail.com>
2023-03-08 00:12:05 +0300

    js types

---
+++ js-course/js-types/index.js
@@ -0,0 +1,74 @@
+// types
+
+// 1. string 'a$#;,?*s' "hello" `234`
+// 2. number 23 2.5 -120 NaN Infinity
+// 3. undefined undefined
+// 4. null null
+// 5. boolean true false 
+// 6. object {}
+// 7. bigint
+// 8. symbol
+
+
+// console.log( typeof null );
+
+// > < >= <= == === != !== 
+
+
+// в строку
+// String();
+// 2 + ''
+
+// в булевый
+// Boolean();
+// !!2
+
+// в число
+// Number()
+// +false
+
+
+// let a = 2;
+// let b = 3;
+
+// a = a + b;
+// a += b;
+
+// a = a - b;
+// a -= b;
+
+// a = a * b;
+// a *= b;
+
+// a = a / b;
+// a /= b;
+
+// let a = 1;
+
+// console.log(++a);
+// console.log(a);
+
+
+// let b = a++ + 2;
+
+// a += b;
+
+// let b = a + 2;
+// a = a + 1;
+
+// console.log(b);
+// console.log(a);
+
+
+
+// for (let i = 0; i < 10; ++i) {
+//     console.log(i);
+// }
+
+// const b = a++ - --a;
+
+// let a = 2;
+
+// while (a < 10) {
+//     console.log(a++);
+// }
