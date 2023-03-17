65e9df502f19fc68014b4e52d2b4de2c7d7eab2f
Prepod <vlad.it.instructor@gmail.com>
2023-03-17 12:18:16 +0300

    arrays

--- js-course/arrays/index.js
+++ js-course/arrays/index.js
@@ -19,18 +19,132 @@
 //     [7,8,9]
 // ];
 
-
-// console.log(  arr[1][2] );
-
-
-
-
+// console.log( arr[1][2] );
 // console.log( [1,2,3][1] );
 
 
+// const arr = new Array(5);
+// console.log(arr);
 
 
+// const arr = ['John', 'Anna', 'Jane', 'Elena', 'Peter'];
+
+// const arr2 = arr;
+
+// const arr3 = [1,2,3];
+
+// console.log( arr2 === arr );
 
 
+// const arr = ['John', 'Anna', 'Jane', 'Elena', 'Peter'];
+
+// const arr2 = [];
+
+// for (let i = 0; arr2.length !== 5; i++) {
+//    arr2[i] = i;
+// }
+
+// console.log(arr2);
+
+// const str = 'hello';
+
+// for (item of str) {
+//     console.log(item);
+// }
 
 
+// const arr = ['John', 'Anna', 'Jane', 'Elena', 'Peter'];
+
+// const arr2 = [1,2,3];
+
+// .push()
+// const res = arr.push('Valera', 'Katya');
+
+// .unshift()
+// const res = arr.unshift('Katya', 'Valera');
+
+// .pop()
+// const res = arr.pop();
+
+// .shift()
+// const res = arr.shift();
+
+// arr2.push(arr.pop());
+
+// const arr = ['John', 'Anna', 'Jane', 'Elena', 'Peter'];
+// const arr2 = [1,2,3];
+
+// .concat()
+// const res = arr.concat('asdsad', 2);
+// const res = arr.concat();
+
+// console.log(arr);
+
+// console.log(res === arr);
+
+// .slice()
+// const res = arr.slice();
+
+// const arr = ['John', 'Anna', 'Jane', 'Elena', 'Peter', 'Anna'];
+// const arr2 = [5,2,4];
+
+
+// const res = arr.includes('Anna');
+// const res = arr.indexOf('asdsad', 2);
+// const res2 = arr.lastIndexOf('Anna', 4);
+// const res = arr.reverse();
+// const res = arr.sort();
+// const res = arr.join(' ');
+// const res = arr.splice(3, 2, 'Bob', 'Bob', 'Bob');
+
+// ВНИМАНИЕ! МЕТОД СТРОКИ!
+// console.log( str.split(' ').join('-') );
+// const str = 'I like JS very much!';
+
+
+// const arr = ['John', 'Anna', 'Jane', 'Elena', 'Peter', 'Anna'];
+
+// 1.
+// const arr = [3,6,3,7,3,41,51,76,23]; // for of
+// найти сумму всех элементов
+
+// 2. Найсти среднее арифметическое всех элементов 
+// массива которые стоят на четных местах // for of ==>> for
+
+
+// 1.
+// const arr = [3,6,3,7,3,41,51,76,23]; 
+// let sum = 0;
+
+// for (let item of arr) {
+//     sum += item;
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+
+// console.log(sum);
+
+
+// 2.
+
+// let sum = 0;
+// let counter = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if ( i % 2 === 0 ) {
+//         sum += arr[i];
+//         counter++;
+//     }
+// }
+
+
+// console.log( sum/counter );
+
+
+// const arr = [3,6,3,7,3,41,51,76,23]; 
+
+// for (let item of arr) {
+//     console.log(item);
+// }
---
+++ js-course/homeworks/arrays/readme.md
@@ -0,0 +1,39 @@
+# Arrays
+
+1. Дан массив 
+`["John", "Peter", "David", "Lambert", "Eddie", "Janett", "Franz"];`
+Вырезать из массива "David", "Lambert", "Eddie" и записать их в отдельный массив
+
+2. Дан массив
+`["John", "Peter", "David", "Lambert", "Eddie", "Janett", "Franz"];`
+Переставить "Janett", "Franz" в начало массива
+
+3. Дан массив 
+`["John", "Peter", "David", "Lambert", "Eddie", "Janett", "Franz"];`
+Заменить "Eddie", "Janett" на "Oleg","Valerchik"
+
+4. Дан массив 
+`["John", "Peter", "David", "Lambert", "Eddie", "Janett", "Franz"];`
+"Перевернуть" массив, заменить первые 4 элемента на Митрофанов ("Mitrofan")
+
+5. Даны два массива 
+`["John", "Peter", "David", "Lambert", "Eddie", "Janett", "Franz"]`
+`["Nikodim","Evlampij","Aristarkh","Drogolub","Vitalij"]`
+Взять из второго массива все имена начина с "Aristarkh" (включительно)
+и добавить их в 1ый массив (не должно получится двумерного массива)
+
+6. Дана строка
+`'Мама мыла раму'`
+С помощью `prompt()` получите строку.
+Поменяйте в строке `'Мама мыла раму'` слово `раму` на полученное слово
+Например:
+```
+prompt() ===>>> 'машину'
+// ‘Мама мыла машину’
+prompt() ===>>> 'рябину'
+// ‘Мама мыла рябину’
+prompt() ===>>> 'картину'
+// ‘Мама мыла картину’
+```
+Вы можете использовать методы split, join, pop, push, slice и
+любые другие
