6366e236f4ba4072f4c25fc2f10561878b7150b0
Prepod <vlad.it.instructor@gmail.com>
2023-05-02 23:09:15 +0300

    error handling

--- js-course/classes/index.js
+++ js-course/classes/index.js
@@ -1,138 +1,143 @@
-// 4. tasks
-'use strict';
 
-// class Person {
-//     constructor (userName = 'Oleg', age = 10) { // userName, age
-//         // this = {}
-//         this.userName = userName;
-//         this.age = age;
 
-//         // return this;
+// set get
+
+// class Car {
+//     #fuelAmount = 0;
+//     podveska = true;
+//     _tankVolume = 50;
+
+//     constructor (color) {
+//         this.color = color;
 //     }
 
-//     run () {
-//         console.log(`${this.userName} runs`);
+//     getFuelAmount () {
+//         return this.#fuelAmount;
 //     }
-// }
 
-// // const p = new Person('Valerchik', 43);
-
-// class Programmer extends Person {
-//     constructor (stack = [],...args) { // stack, userName, age
-//         // this = {}
-//         super(...args); // userName, age
-
-//         this.stack = stack;
+//     set fuelAmount (val) {
+//         if (val <= this.#tankVolume) {
+//             this.#fuelAmount = val;
+//         }
 //     }
-// }
 
-
-// const pr = new Programmer(['JS', 'JAVA'], 'Valerchik', 51 );
-
-// class JSDeveloper extends Programmer {
-//     constructor (smoozyLitres = 10, ...args) { // [stack, userName, age]
-//         super(...args); // stack, userName, age
-//         this.smoozyLitres = smoozyLitres; 
+//     get fuelAmount () {
+//         return this.#fuelAmount;
 //     }
 // }
 
+// const tesla = new Car('red');
+// tesla.podveska = false;
 
-// const jsDev = new JSDeveloper(10, ['React', 'Redux'],'Anna', 51);
-
-
-// const foo = () => {
-//     console.log('runs');
+// Car.repair = function (car) {
+//     car.podveska = true;
 // };
 
-// class Person {
-//     #age;
+// Car.repair(tesla);
 
-//     constructor (userName, age) {
-//         this.userName = userName;
-//         this.#age = age;
-//     }
+// Создать класс Shop, инстанс которого обладает полем
+// shopItems - массив объектов типа shopItem
+// У инстанса класса Shop должен быть метод setSales(discountPercent),
+// который устанавливает всем shopItem'ам isOnSale = true,
+// а также снижает цену на discountPercent процентов.
+// У инстанса также должен быть метод purchaseItem(itemName)
+// которое находит в массиве shopItems, необходимый shopItem и уменьшает его
+// количество на 1.
+// Если количество shopItem равно нулю, то нужно выбросить ошибку о том, что
+// айтемов больше нет в наличии. Обработайте эту ошибку и выведите в консоль
+// сообщение что айтем нельзя заказать тк он закончился.
+// У инстанса Shop также должно быть поле totalProfit - число (общее
+// количество заработанных денег), в которое складывается
+// цена каждого проданного айтема
 
-//     walk () {
-//         console.log('adasdas');
-//     }
+// Создать класс Clothes - который наследует от ShopItem
+// инстанс должен обладать дополнительными полями
+// size - строка характеризующая размер (может принимать значения только 'S', 'M', 'L', 'XL', обрабатывать
+// сеттером, иначе выбрасывать ошибку)
+// material - строка характеризующая материал
+// color - строка цвета
+// Класс Clothes, должен обладать статическим методом .changeColor(cloth, color)
+// который принимает инстанс этого класса и цвет, метод меняет инстансу цвет на color
 
-    // set age (val) { // запись
-    //     if (val > 0 && val < 120) {
-    //         this.#age = val;
-    //     }
-    // }
+// Создать класс TV который наследует от ShopItem
+// инстанс должен обладать дополнительными полями
+// diagonal - число
+// withSmartTV - булево значение
+// Оба поля должны быть недоступны для изменения, но доступны для чтения
+// т.е. значения этих полей устанавливаются только в конструкторе, дальше сеттерами
+// запрещаем их изменят
 
-//     get age () { // чтение
-//         return this.#age;
-//     }
-// }
-
-// const pr = new Person('Anna', 23);
-
-// console.log( pr._age );
+// Создать инстанс Shop и прокинуть в него несколько инстансов TV и Clothes
+// проверить все методы на работоспособност
 
 
-
-// ООП наследование инкапсуляция полиморфизм абстракция
-
-// создать класс Person 
-// с полями firstName (имя)
-// lastName (фамилия)
-// fullName (имя + фамилия)
-// можно читать каждое из этих полей
-// если поменять lastName (или firstName)
-// то поле fullName ТОЖЕ ДОЛЖНО ПОМЕНЯТЬСЯ (без дополнительных записей)
-
-// person.lastName = 'Ivanov';
-
-// person.fullName ===>>> Valerchik Ivanov
-
-// person.lastName = 'Sidorov';
-// // person.fullName = ????.split('')[0] = 'Sidorov'
-
-// person.fullName ===>>> Valerchik Sidorov
-
-class Person{
-    constructor(userName,lastName){
-        this.firstName=userName;
-        this.lastName=lastName;
-    }
-
-    get fullName () {
-        return this.firstName + ' ' + this.lastName;
-    }
-
-    set fullName (val) { // 'Ivan Ivanov'
-        const arrName = val.split(' '); // ['Ivan', 'Ivanov']
-        this.firstName = arrName[0];
-        this.lastName = arrName[1];
+class ShopItem {
+    constructor (itemName, price) {
+        this.itemName = itemName;
+        this.price = price;
+        // set get throw new ShopItemAmountError('some text here')
+        this.amount = 3; 
+        this.isOnSale = false;
     }
 }
 
-function foo () {
-    throw new Error();
+class Clothes extends ShopItem {
+    #size;
+
+    constructor (size, material, color = '#fff',...args) {
+        super(...args);
+        this.size = size;
+        this.material = material;
+        this.color = color;
+    }
+
+    get size () {
+        return this.#size;
+    }
+
+    set size (passedSize) {
+        const validSizes = ['S', 'M', 'L', 'XL'];
+        const isSizeValid = validSizes.some((validSize) => validSize === passedSize.toUpperCase());
+
+        if (isSizeValid) {
+            this.#size = passedSize;
+        } else {
+            throw new Error(`size should be either 'S', 'M', 'L', 'XL'`)
+        }
+    }
 }
 
-// TODO
-// try catch 
-// static method class
-// заимстовавание методов
-// import export (js module) ???
+const maika = new Clothes('XL', 'raps', 'orange', 'maika', 200);
+console.log(maika);
 
-
-
-// foo();
-
-// const user = new Person('Maks','Jacson');
-
-// user.lastName = 'Ivanov';
-
-// function Person (userName,lastName) {
-//     // this = {}
-//     this.firstName=userName;
-//     this.lastName=lastName;
-//     return this;
+// let person = {
+//     userName: 'Oleg'
 // }
 
-// console.log( Person());
+// class PersonAgeError extends Error {}
+
+// function makeValerchik (person = {}, age) {
+//     if (isNaN(age) || typeof age !== 'number') {
+//         throw new PersonAgeError(`argument age was expected to be a number, but instead got ${age}`);
+//     }
+//     person.userName = 'Valerchik';
+// }
+
+
+// try {
+
+//     const a = 2;
+//     a = 3;
+//     makeValerchik(person, null);
+// } catch (err) {
+//     console.log(err);
+//     if (err instanceof PersonAgeError) {
+//         makeValerchik(person, 20);
+//     } else {
+//         throw err;
+//     }
+// }
+
+
+// console.log(person);
 
