19d018f595b0dcfddb701540e97257a1e52cb855
Prepod <vlad.it.instructor@gmail.com>
2023-05-04 23:15:48 +0300

    classes 3

--- js-course/classes/index.js
+++ js-course/classes/index.js
@@ -1,7 +1,5 @@
-
-
 // set get
-
+'use strict';
 // class Car {
 //     #fuelAmount = 0;
 //     podveska = true;
@@ -35,6 +33,177 @@
 
 // Car.repair(tesla);
 
+// 2. 3. 6.
+
+// class Programmer {
+//   constructor(stack, name = "Valerchik") {
+//     this.name = name;
+//     this.stack = stack;
+//   }
+
+//   code() {
+//     this.stack.forEach((technology) => {
+//       console.log(`${this.name} codes on ${technology}`);
+//     });
+//   }
+
+//   static getSeniorDeveloper(programmers) {
+//     return programmers.filter((programmer) => {
+//         return programmer.stack.length > 4;
+//     });
+//   }
+// }
+
+// const programmers = [
+//   new Programmer(["PHP", "Java", "Paint"], "Oleg"),
+//   new Programmer(["JS", "CSS", "TS", "React", "Angular"], "Anna"),
+//   new Programmer(["czxczx", "sdxzc", "asdsa", "sadzczv", "xzcxcxv"]),
+//   new Programmer(["dksfjl", "sad", "xzc"]),
+// ];
+
+// const seniours = Programmer.getSeniorDeveloper(programmers);
+
+
+
+
+
+// class Programmer {
+//   constructor(userName, stack) {
+//     this.userName = userName;
+//     this.stack = stack;
+//     Programmer.prototype.code = function () {
+//       stack.forEach((element) => {
+//         console.log(`${this.userName} is coding on ${element}`);
+//       });
+//       Programmer.prototype.toString = function prToString() {
+//         console.log(`Hello my name is ${this.userName} `);
+//       };
+//     };
+//   }
+//   static getSeniorDeveloper(programmerTeam) {
+//     return programmerTeam.filter((programmer) => {
+//         return programmer.stack.length > 4;
+//     });
+//   }
+// }
+
+
+// const programmerTeam = [
+//     new Programmer("Petya", ["JS", "Python", "Go", "Ruby"]),
+//     new Programmer("Vlad", ["Java", "PHP"]),
+//     new Programmer("Olga", ["CSS", "HTMl"]),
+//     new Programmer("Valerchik", ["Assembler", "API", "C++", "C#", "PHP"]),
+//     new Programmer("Artem", ["C++", "C#"]),
+//     new Programmer("Vova", ["Go", "Ruby"]),
+//     new Programmer("Sergei", ["SQL", "Swift"]),
+//     new Programmer("Anya", ["TypeScript", "React", "JS", "CSS", "HTML"]),
+//     new Programmer("Roman", ["Delphi", "Scala"]),
+//     new Programmer("Maksim", ["Kotlin", "Golang"]),
+//   ];
+
+// console.log(Programmer.getSeniorDeveloper(programmerTeam));
+
+
+
+// 3. Напишите функцию, которая вычисляет длину линий в
+// стандартной двухмерной системе координат. Координаты
+// начала и конца линий должны храниться в объектах.
+// - Создайте три линии.
+// - Вычислите их длины с помощью функции
+// - Проверьте, могут ли три линии сформировать треугольник
+// **Помните:**
+// Длину линии можно найти по теореме Пифагора.
+// Стороны могут сформировать треугольник если длина
+// каждой стороны треугольника должны быть меньше суммы
+// длин остальных сторон.
+
+
+// class Point {
+//     constructor (x,y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+// }
+
+// const p1 = new Point(51,123);
+// const p2 = new Point(16, 98);
+// const p3 = new Point(76, 5);
+// const p4 = new Point(4235, 18);
+// const p5 = new Point(16, 63);
+// const p6 = new Point(55, 34);
+
+// class Line {
+//     constructor (p1, p2) {
+//         this.p1 = p1;
+//         this.p2 = p2;
+//     }
+
+//     get length () {
+//         const katet1 = this.p1.x - this.p2.x;
+//         const katet2 = this.p1.y - this.p2.y;
+        
+//         return Math.sqrt(katet1**2 + katet2**2);
+//     }
+
+//     static canLinesFormATriangle (line1, line2, line3) {
+//         return (line1.length + line2.length > line3.length &&
+//             line2.length + line3.length > line1.length &&
+//             line3.length + line1.length > line2.length);
+        
+//     }
+// }
+
+// const line1 = new Line(p1, p2);
+// const line2 = new Line(p3, p4);
+// const line3 = new Line(p5, p6);
+
+
+// console.log(Line.canLinesFormATriangle(line1, line2, line3));
+
+
+// Создать класс `Timer`, инстанс которого запускает таймер и выводит в консоль
+// прошедшее время в секундах (1 2 3 4 5 и тд)
+// инстанс должен обладать методами `.start()` `.stop()` которые запускают и останавливают таймер
+// а также методом `.reset()` который сбрасывает таймер на 0.
+// С помощью Timer вы должны создать несколько таймеров и убедится в том, что они 
+// работают независимо друг от друга.
+// останавливайте таймер через некоторое время после запуска с помощью `setTimeout()`
+
+// class Timer {
+//     constructor () {
+//         this.intervalId = null;
+//         this.time = 0;
+//     }
+
+//     start () {
+//         this.intervalId = setInterval(() => {
+//             console.log(++this.time);
+//         }, 1000);
+//     }
+
+//     stop () {
+//         clearInterval(this.intervalId);
+//     }
+
+//     reset () {
+//         this.time = 0;
+//     }
+// }
+
+// const timer = new Timer();
+
+// console.log(timer);
+
+// timer.start();
+
+// setTimeout(() => {
+//     timer.reset();
+// }, 5000);
+
+// setTimeout(() => {
+//     timer.stop();
+// }, 7000);
+
 // Создать класс Shop, инстанс которого обладает полем
 // shopItems - массив объектов типа shopItem
 // У инстанса класса Shop должен быть метод setSales(discountPercent),
@@ -50,19 +219,7 @@
 // количество заработанных денег), в которое складывается
 // цена каждого проданного айтема
 
-// Создать класс Clothes - который наследует от ShopItem
-// инстанс должен обладать дополнительными полями
-// size - строка характеризующая размер (может принимать значения только 'S', 'M', 'L', 'XL', обрабатывать
-// сеттером, иначе выбрасывать ошибку)
-// material - строка характеризующая материал
-// color - строка цвета
-// Класс Clothes, должен обладать статическим методом .changeColor(cloth, color)
-// который принимает инстанс этого класса и цвет, метод меняет инстансу цвет на color
-
 // Создать класс TV который наследует от ShopItem
-// инстанс должен обладать дополнительными полями
-// diagonal - число
-// withSmartTV - булево значение
 // Оба поля должны быть недоступны для изменения, но доступны для чтения
 // т.е. значения этих полей устанавливаются только в конструкторе, дальше сеттерами
 // запрещаем их изменят
@@ -72,43 +229,91 @@
 
 
 class ShopItem {
-    constructor (itemName, price) {
-        this.itemName = itemName;
-        this.price = price;
-        // set get throw new ShopItemAmountError('some text here')
-        this.amount = 3; 
-        this.isOnSale = false;
+    #amount = 3;
+
+  constructor(itemName, price) {
+    this.itemName = itemName;
+    this.price = price;
+    this.isOnSale = false;
+  }
+
+  set amount (val) {
+    if (val < 0) {
+        throw new Error('amount cannot be under zero');
     }
+
+    this.#amount = val;
+  }
+
+  get amount () {
+    return this.#amount;
+  }
 }
 
 class Clothes extends ShopItem {
-    #size;
+  #size;
 
-    constructor (size, material, color = '#fff',...args) {
-        super(...args);
-        this.size = size;
-        this.material = material;
-        this.color = color;
+  constructor(size, material, color = "#fff", ...args) {
+    super(...args);
+    this.size = size;
+    this.material = material;
+    this.color = color;
+  }
+
+  get size() {
+    return this.#size;
+  }
+
+  set size(passedSize) {
+    const validSizes = ["S", "M", "L", "XL"];
+    const isSizeValid = validSizes.some(
+      (validSize) => validSize === passedSize.toUpperCase()
+    );
+
+    if (isSizeValid) {
+      this.#size = passedSize;
+    } else {
+      throw new Error(`size should be either 'S', 'M', 'L', 'XL'`);
     }
+  }
 
-    get size () {
-        return this.#size;
-    }
-
-    set size (passedSize) {
-        const validSizes = ['S', 'M', 'L', 'XL'];
-        const isSizeValid = validSizes.some((validSize) => validSize === passedSize.toUpperCase());
-
-        if (isSizeValid) {
-            this.#size = passedSize;
-        } else {
-            throw new Error(`size should be either 'S', 'M', 'L', 'XL'`)
-        }
-    }
+  static changeColor (cloth, color) {
+    cloth.color = color;
+  }
 }
 
-const maika = new Clothes('XL', 'raps', 'orange', 'maika', 200);
-console.log(maika);
+const maika = new Clothes("XL", "raps", "orange", "maika", 200);
+
+class TV extends ShopItem {
+    #diagonal;
+    #withSmartTV;
+
+    constructor (diagonal, withSmartTV, ...args) {
+        super(...args);
+        this.#diagonal = diagonal;
+        this.#withSmartTV = withSmartTV;
+    }
+
+    get diagonal () {
+        return this.#diagonal;
+    }
+
+    set diagonal (val) {
+        throw new Error ('property diagonal is readonly')
+    }
+
+    get withSmartTV () {
+        return this.#withSmartTV;
+    }
+
+    set withSmartTV (val) {}
+}
+
+const lg = new TV(55, false, 'lg', 2000);
+
+lg.diagonal = 100;
+
+console.log( lg.diagonal );
 
 // let person = {
 //     userName: 'Oleg'
@@ -122,7 +327,6 @@ console.log(maika);
 //     }
 //     person.userName = 'Valerchik';
 // }
-
 
 // try {
 
@@ -138,6 +342,4 @@ console.log(maika);
 //     }
 // }
 
-
 // console.log(person);
-
